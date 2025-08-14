57f5f44aa5d10bf6d9097090a48a6df6ba920acb
Prasanth <prasanth@kavia.ai>
2025-08-14 09:15:09 +0530

    CheckPoint - cgcdb2b17e

---
+++ hello_world_backend/src/test/java/com/example/demo/helloworldbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworldbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class helloworldbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
